461971ec7b3bc1095b8ac92dd779e7c6a7af4ca5
skchauhan <skrajput42@gmail.com>
2017-12-25 09:09:07 +0530

    first upload package to github

---
+++ src/GoogleServiceProvider.php
@@ -0,0 +1,27 @@
+<?php
+namespace Chauhan\Google;
+
+use Illuminate\Support\ServiceProvider;
+
+class GoogleServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->loadRoutesFrom(__DIR__.'/routes.php');
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
---
+++ src/routes.php
@@ -0,0 +1,5 @@
+<?php 
+	Route::get('facebook', function(){
+		return "testing";
+	});
+?>
